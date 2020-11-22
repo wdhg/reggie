88ea7a25044f6ecb3ae50cd5b052b47f86dfc9e3
wdhg <wdhgrant@gmail.com>
2020-11-22 22:05:13 +0000

    simplify data structures

--- src/Program.hs
+++ src/Program.hs
@@ -1,27 +1,15 @@
 module Program where
 
-newtype ID a
-  = ID Int
-    deriving (Show, Eq)
-
 newtype Register
   = Register Int
     deriving Show
 
-newtype Registers
-  = Registers [(ID Register, Register)]
-    deriving Show
-
 data Instruction
-  = Incr (ID Register) (ID Instruction)
-  | Decr (ID Register) (ID Instruction, ID Instruction)
+  = Incr Int Int
+  | Decr Int (Int, Int)
   | Halt
     deriving Show
 
-newtype Program
-  = Program [Instruction]
-    deriving Show
-
-data MachineState
-  = MachineState Registers Program
-    deriving Show
+run :: [Instruction] -> [Register]
+run
+  = undefined
