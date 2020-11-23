5efdd953752454ff7c457b91d41e1cb568aed8af
wdhg <wdhgrant@gmail.com>
2020-11-23 14:18:01 +0000

    add instance Encode for Instruction

--- src/Encode.hs
+++ src/Encode.hs
@@ -2,6 +2,8 @@
 
 module Encode where
 
+import Program (Instruction(..))
+
 class Encode a where
  -- Bijection from NxN to N+
   encode :: Integral b => a -> b
@@ -17,3 +19,11 @@ instance (Integral a, Integral b) => Encode (a, b) where
 instance Encode a => Encode [a] where
   encode
     = foldr (\x y -> encode (encode x, y)) 0
+
+instance Encode Instruction where
+  encode (Incr reg next)
+    = encode (2 * reg, next)
+  encode (Decr reg nexts)
+    = encode (2 * reg + 1, encode' nexts)
+  encode Halt
+    = 0
