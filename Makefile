c9201d579965d4d58b2ae6fdc2b4aebf0baf243f
wdhg <wdhgrant@gmail.com>
2020-11-23 17:11:14 +0000

    add command line arguments

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 all: src/Tokens.x src/Grammer.y
 	alex src/Tokens.x
 	happy src/Grammer.y
+
+clean:
+	rm -rf src/Tokens.hs src/Grammer.hs
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import System.Environment
+import Tokens
+import Grammer
+import Program
+import Encode
+
 main :: IO ()
-main = undefined
+main
+  = do
+    args <- getArgs
+    case args of
+      ["run", prog] -> do
+        contents <- readFile prog
+        print $ run $ parse $ tokenize contents
+      ["encode", prog] -> do
+        contents <- readFile prog
+        print $ encode $ parse $ tokenize contents
+      _ -> do
+        name <- getProgName
+        putStrLn $ "Usage: " ++ name ++ " [run|encode] <program>"
