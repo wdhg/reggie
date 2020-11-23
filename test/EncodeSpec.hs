61d6fe8629732f0d8b6b94b596fea18f69ef11a5
wdhg <wdhgrant@gmail.com>
2020-11-23 15:35:42 +0000

    test encoding instructions and programs

--- test/EncodeSpec.hs
+++ test/EncodeSpec.hs
@@ -2,12 +2,13 @@ module EncodeSpec (spec) where
 
 import Test.Hspec
 import Encode
+import Program (Instruction(..))
 
 spec :: Spec
 spec
   = describe "Encode" $ do
       describe "encode" $ do
-        it "can encode pairs correctly" $ do
+        it "can encode pairs" $ do
           encode (0 :: Int, 0:: Int) `shouldBe` 1
           encode (1 :: Int, 0 :: Int) `shouldBe` 2
           encode (5 :: Int, 0 :: Int) `shouldBe` 32
@@ -15,6 +16,17 @@ spec
           encode (1 :: Int, 2 :: Int) `shouldBe` 10
           encode (2 :: Int, 1 :: Int) `shouldBe` 12
           encode (2 :: Int, 2 :: Int) `shouldBe` 20
+
+        it "can encode instructions" $ do
+          encode (Incr 0 0) `shouldBe` 1
+          encode (Incr 4 6) `shouldBe` 3328
+          encode (Decr 0 (0, 0)) `shouldBe` 2
+          encode (Decr 3 (1, 8)) `shouldBe` 8576
+          encode Halt `shouldBe` 0
+
+        it "can encode programs" $ do
+          encode [Halt] `shouldBe` 1
+          encode [Decr 0 (0, 2), Halt] `shouldBe` 786432
 
       describe "encode'" $ do
          it "can encode pairs correctly" $ do
