514998033fa6f6d25f9e20332c5f06a074b5108c
wdhg <wdhgrant@gmail.com>
2020-11-23 11:47:58 +0000

    implement run and write tests

--- src/Program.hs
+++ src/Program.hs
@@ -4,8 +4,43 @@ data Instruction
   = Incr Int Int
   | Decr Int (Int, Int)
   | Halt
-    deriving Show
+    deriving (Show, Eq)
 
 run :: [Instruction] -> [(Int, Int)]
-run
-  = undefined
+run program
+  = let (_, _, registers) = until halting step (0, program, [])
+     in registers
+
+halting :: (Int, [Instruction], [(Int, Int)]) -> Bool
+halting (pc, program, _)
+  = (program !! pc) == Halt
+
+step :: (Int, [Instruction], [(Int, Int)]) -> (Int, [Instruction], [(Int, Int)])
+step (pc, program, registers)
+  = case program !! pc of
+      Incr reg next               -> (next, program, incr reg registers)
+      Decr reg (nextIf, nextElse) -> let registers' = decr reg registers
+                                      in if positive reg registers'
+                                            then (nextIf, program, registers')
+                                            else (nextElse, program, registers')
+      Halt                        -> (pc, program, registers)
+
+incr :: Int -> [(Int, Int)] -> [(Int, Int)]
+incr key []
+  = [(key, 1)]
+incr key ((k, v) : remaining)
+  | key == k  = (k, v + 1) : remaining
+  | otherwise = (k, v) : incr key remaining
+
+decr :: Int -> [(Int, Int)] -> [(Int, Int)]
+decr key []
+  = [(key, -1)]
+decr key ((k, v) : remaining)
+  | key == k = (k, v - 1) : remaining
+  | otherwise = (k, v) : decr key remaining
+
+positive :: Int -> [(Int, Int)] -> Bool
+positive reg registers
+  = case lookup reg registers of
+      Just x  -> x > 0
+      Nothing -> False
--- test/ProgramSpec.hs
+++ test/ProgramSpec.hs
@@ -12,3 +12,17 @@ spec
 
       it "can increment values" $ do
         run [Incr 0 1, Halt] `shouldBe` [(0, 1)]
+        run [Incr 1 1, Halt] `shouldBe` [(1, 1)]
+        run [Incr 0 1, Incr 0 2, Halt] `shouldBe` [(0, 2)]
+        run [Incr 0 1, Incr 1 2, Halt] `shouldBe` [(0, 1), (1, 1)]
+
+      it "can decrement values" $ do
+        run [Decr 0 (1,1), Halt] `shouldBe` [(0, -1)]
+        run [Decr 0 (1,1), Decr 0 (2, 2), Halt] `shouldBe` [(0, -2)]
+        run [Decr 0 (1,1), Decr 1 (2, 2), Halt] `shouldBe` [(0, -1), (1, -1)]
+
+      it "can branch on decrement" $ do
+        run [Decr 0 (1, 2), Incr 1 2, Halt] `shouldBe` [(0, -1)]
+        run [Incr 0 1, Decr 0 (2, 3), Incr 1 0, Halt] `shouldBe` [(0, 0)]
+        run [Incr 0 1, Incr 0 2, Incr 0 3, Decr 0 (4, 5), Incr 1 3, Halt]
+          `shouldBe` [(0, 0), (1, 2)]
