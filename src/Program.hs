8ce7429d9476b9c841f0c1a643c8ac96f0f9fb7f
wdhg <wdhgrant@gmail.com>
2020-11-26 10:30:45 +0000

    refactor program code

--- src/Program.hs
+++ src/Program.hs
@@ -1,46 +1,68 @@
 module Program where
 
+import Prelude hiding (lookup)
+import Data.Map (Map(..), alter, empty, lookup, toAscList)
+
 data Instruction
   = Incr Int Int
   | Decr Int (Int, Int)
   | Halt
     deriving (Show, Eq)
 
+data Machine
+  = Machine [Instruction] (Map Int Int) Int
+
 run :: [Instruction] -> [(Int, Int)]
 run program
-  = let (_, _, registers) = until halting step (0, program, [])
-     in registers
+  = let (Machine _ memory _) = until finished step $ Machine program empty 0
+     in toAscList memory
 
-halting :: (Int, [Instruction], [(Int, Int)]) -> Bool
-halting (pc, program, _)
-  = (program !! pc) == Halt
+finished :: Machine -> Bool
+finished (Machine program _ pc)
+  = case getInstr program pc of
+      Nothing    -> True
+      Just instr -> instr == Halt
 
-step :: (Int, [Instruction], [(Int, Int)]) -> (Int, [Instruction], [(Int, Int)])
-step (pc, program, registers)
-  = case program !! pc of
-      Incr reg next               -> (next, program, incr reg registers)
-      Decr reg (nextIf, nextElse) -> let registers' = decr reg registers
-                                      in if positive reg registers'
-                                            then (nextIf, program, registers')
-                                            else (nextElse, program, registers')
-      Halt                        -> (pc, program, registers)
+step :: Machine -> Machine
+step machine@(Machine program _ pc)
+  = maybe machine (`step'`machine) $ getInstr program pc
 
-incr :: Int -> [(Int, Int)] -> [(Int, Int)]
-incr key []
-  = [(key, 1)]
-incr key ((k, v) : remaining)
-  | key == k  = (k, v + 1) : remaining
-  | otherwise = (k, v) : incr key remaining
+step' :: Instruction -> Machine -> Machine
+step' (Incr reg next) (Machine program memory _)
+  = Machine program (alter incr reg memory) next
+step' (Decr reg nexts) (Machine program memory _)
+  = let memory' = alter decr reg memory
+        next = pickNext (regPositive reg memory') nexts
+     in Machine program memory' next
+step' Halt machine
+  = machine
 
-decr :: Int -> [(Int, Int)] -> [(Int, Int)]
-decr key []
-  = [(key, -1)]
-decr key ((k, v) : remaining)
-  | key == k = (k, v - 1) : remaining
-  | otherwise = (k, v) : decr key remaining
+getInstr :: [Instruction] -> Int -> Maybe Instruction
+getInstr [] _
+  = Nothing
+getInstr (instr : _) 0
+  = Just instr
+getInstr (_ : program) pc
+  = getInstr program (pc - 1)
 
-positive :: Int -> [(Int, Int)] -> Bool
-positive reg registers
-  = case lookup reg registers of
-      Just x  -> x > 0
+regPositive :: Int -> Map Int Int -> Bool
+regPositive reg memory
+  = case lookup reg memory of
       Nothing -> False
+      Just x  -> x > 0
+
+pickNext :: Bool -> (Int, Int) -> Int
+pickNext True (x, _)  = x
+pickNext False (_, y) = y
+
+incr :: Maybe Int -> Maybe Int
+incr Nothing
+  = Just 1
+incr (Just x)
+  = Just $ succ x
+
+decr :: Maybe Int -> Maybe Int
+decr Nothing
+  = Just (-1)
+decr (Just x)
+  = Just $ pred x
