a141afe769f24779c77bf87fe85826012e27008a
wdhg <wdhgrant@gmail.com>
2020-11-28 10:33:37 +0000

    get tests working again

--- test/EncodeSpec.hs
+++ test/EncodeSpec.hs
@@ -2,14 +2,27 @@ module EncodeSpec (spec) where
 
 import Test.Hspec
 import Encode
-import Program (Instruction(..))
+import Program (Program(..), Instruction(..), Label(..))
+import Data.Map (fromList)
+
+incr :: Integer -> Integer -> Instruction
+incr reg next
+  = Incr reg (Label next)
+
+decr :: Integer -> (Integer, Integer) -> Instruction
+decr reg (nextThen, nextElse)
+  = Decr reg (Label nextThen, Label nextElse)
+
+program :: [Instruction] -> Program
+program
+  = Program . fromList . zip [Label 0..]
 
 spec :: Spec
 spec
   = describe "Encode" $ do
       describe "encode" $ do
         it "can encode pairs" $ do
-          encode (0 :: Int, 0:: Int) `shouldBe` 1
+          encode (0 :: Int, 0 :: Int) `shouldBe` 1
           encode (1 :: Int, 0 :: Int) `shouldBe` 2
           encode (5 :: Int, 0 :: Int) `shouldBe` 32
           encode (0 :: Int, 3 :: Int) `shouldBe` 7
@@ -18,15 +31,15 @@ spec
           encode (2 :: Int, 2 :: Int) `shouldBe` 20
 
         it "can encode instructions" $ do
-          encode (Incr 0 0) `shouldBe` 1
-          encode (Incr 4 6) `shouldBe` 3328
-          encode (Decr 0 (0, 0)) `shouldBe` 2
-          encode (Decr 3 (1, 8)) `shouldBe` 8576
+          encode (incr 0 0) `shouldBe` 1
+          encode (incr 4 6) `shouldBe` 3328
+          encode (decr 0 (0, 0)) `shouldBe` 2
+          encode (decr 3 (1, 8)) `shouldBe` 8576
           encode Halt `shouldBe` 0
 
         it "can encode programs" $ do
-          encode [Halt] `shouldBe` 1
-          encode [Decr 0 (0, 2), Halt] `shouldBe` 786432
+          encode (program [Halt]) `shouldBe` 1
+          encode (program [decr 0 (0, 2), Halt]) `shouldBe` 786432
 
       describe "encode'" $ do
          it "can encode pairs correctly" $ do
--- test/ProgramSpec.hs
+++ test/ProgramSpec.hs
@@ -1,28 +1,41 @@
 module ProgramSpec (spec) where
 
 import Test.Hspec
-import Program
+import Program (Program(..), Instruction(..), Label(..), run)
+import Data.Map (fromList)
+
+incr :: Integer -> Integer -> Instruction
+incr reg next
+  = Incr reg (Label next)
+
+decr :: Integer -> (Integer, Integer) -> Instruction
+decr reg (nextThen, nextElse)
+  = Decr reg (Label nextThen, Label nextElse)
+
+program :: [Instruction] -> Program
+program
+  = Program . fromList . zip [Label 0..]
 
 spec :: Spec
 spec
   = do
     describe "run" $ do
       it "can halt" $
-        run [Halt] `shouldBe` []
+        run (program [Halt]) `shouldBe` []
 
       it "can increment values" $ do
-        run [Incr 0 1, Halt] `shouldBe` [(0, 1)]
-        run [Incr 1 1, Halt] `shouldBe` [(1, 1)]
-        run [Incr 0 1, Incr 0 2, Halt] `shouldBe` [(0, 2)]
-        run [Incr 0 1, Incr 1 2, Halt] `shouldBe` [(0, 1), (1, 1)]
+        run (program [incr 0 1, Halt]) `shouldBe` [(0, 1)]
+        run (program [incr 1 1, Halt]) `shouldBe` [(1, 1)]
+        run (program [incr 0 1, incr 0 2, Halt]) `shouldBe` [(0, 2)]
+        run (program [incr 0 1, incr 1 2, Halt]) `shouldBe` [(0, 1), (1, 1)]
 
       it "can decrement values" $ do
-        run [Decr 0 (1,1), Halt] `shouldBe` [(0, -1)]
-        run [Decr 0 (1,1), Decr 0 (2, 2), Halt] `shouldBe` [(0, -2)]
-        run [Decr 0 (1,1), Decr 1 (2, 2), Halt] `shouldBe` [(0, -1), (1, -1)]
+        run (program [decr 0 (1,1), Halt]) `shouldBe` [(0, -1)]
+        run (program [decr 0 (1,1), decr 0 (2, 2), Halt]) `shouldBe` [(0, -2)]
+        run (program [decr 0 (1,1), decr 1 (2, 2), Halt]) `shouldBe` [(0, -1), (1, -1)]
 
       it "can branch on decrement" $ do
-        run [Decr 0 (1, 2), Incr 1 2, Halt] `shouldBe` [(0, -1)]
-        run [Incr 0 1, Decr 0 (2, 3), Incr 1 0, Halt] `shouldBe` [(0, 0)]
-        run [Incr 0 1, Incr 0 2, Incr 0 3, Decr 0 (4, 5), Incr 1 3, Halt]
+        run (program [decr 0 (1, 2), incr 1 2, Halt]) `shouldBe` [(0, -1)]
+        run (program [incr 0 1, decr 0 (2, 3), incr 1 0, Halt]) `shouldBe` [(0, 0)]
+        run (program [incr 0 1, incr 0 2, incr 0 3, decr 0 (4, 5), incr 1 3, Halt])
           `shouldBe` [(0, 0), (1, 2)]
